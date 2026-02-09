52cfdbf84eb410083c1f02318d02cbef9b47b196
oluwadamilareakinyele <oluwadamilareakinyele@oluwadalaresair.communityfibre.co.uk>
2026-02-09 18:02:06 +0000

    first comment...

---
+++ gittest/src/gittest/javatestgit.java
@@ -0,0 +1,23 @@
+package gittest;
+
+public class javatestgit {
+	
+	
+	public void test()
+	{
+	
+		String  yemi = "leader";
+	
+	if(yemi=="leader")
+	{
+		System.out.println("correct");
+	}
+	else
+	{
+		System.out.println("not leader");
+	}
+
+}
+
+}
+
